632d8200d2ed7174d3b59d53dae66026a8f9548a
lottaberglund <csb1073@usnh.edu>
2024-06-28 11:28:31 -0400

    Add files via upload

--- lberglund.shapes.c
+++ lberglund.shapes.c
@@ -17,5 +17,19 @@ int main()
         printf("(%.2f, %.2f)\n", CirclexCoors, CircleyCoors);
     }
 
+    int SquarexCenter = 11;
+    int SquareyCenter = 1;
+    int SquareRadius = 1;
+    int i;
+
+    int CoorsPointsx[] = {SquarexCenter + SquareRadius, SquarexCenter, SquarexCenter - SquareRadius, SquarexCenter, SquarexCenter + SquareRadius, SquarexCenter - SquareRadius, SquarexCenter + SquareRadius, SquarexCenter - SquareRadius}; 
+
+    int CoorsPointsy[] = {SquareyCenter, SquareRadius + SquareyCenter, SquareyCenter, SquareyCenter - SquareRadius, SquareyCenter - SquareRadius, SquareyCenter + SquareRadius, SquareyCenter + SquareRadius, SquareyCenter - SquareRadius}; 
+
+    for (i=0; i < 8; i++){
+        printf("(%d, %d)\n", CoorsPointsx[i], CoorsPointsy[i]);
+    }
+
+
     return 0;
 }
